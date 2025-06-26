d0f55d3a9bed800c4d6f1902acc76bbf81d01f7f
adrianbalbs <adrianbalbs@hotmail.com>
2025-06-26 12:16:57 +1000

    initial RAM impl

---
+++ src/lib/ram.rs
@@ -0,0 +1,16 @@
+struct Memory {
+    ram: [u8; 0xFFFF],
+}
+
+/// TODO: I should probably boundary check here shouldn't I...
+/// For now assume we'll pass in valid memory ranges, don't worry about mapping
+/// sections of memory for now
+impl Memory {
+    fn read(&self, addr: u16) -> u8 {
+        self.ram[addr as usize]
+    }
+
+    fn write(&mut self, addr: u16, data: u8) {
+        self.ram[addr as usize] = data;
+    }
+}
